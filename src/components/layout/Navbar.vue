4974e57570aa74d629d58ba4f4222490e5d0e362
Noah Korner <noahskorner@gmail.com>
2021-03-20 13:37:44 -0500

    Completed initial setup

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav" class="w-screen"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#nav {
+  background-color: blue;
+  height: 4rem;
+}
+</style>
